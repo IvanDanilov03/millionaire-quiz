60c43f63181e28b31bff908ac604a4fcaccf7a3f
Ivan <ivd@empat.tech>
2025-10-09 00:46:26 +0300

    feat: update general constants

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,22 +1,8 @@
 export const DEFAULT_TITLE: Record<string, string> = {
-  uk: 'Zvigerauto',
-  ru: 'Zvigerauto',
+  uk: 'Who wants to be a millionaire?',
 };
 
 export const APP_ROUTES = {
   home: '/',
   notFound: '/not-found',
-
-  aboutUs: '/about-us',
-  contactUs: '/contact-us',
-
-  faq: '/faq',
-  termsAndConditions: '/terms-and-conditions',
-  privacyPolicy: '/privacy-policy',
-  cookiesPolicy: '/cookies-policy',
-
-  X: 'https://x.com',
-  linkedin: 'https://www.linkedin.com',
-  instagram: 'https://www.instagram.com',
-  facebook: 'https://www.facebook.com',
 };
