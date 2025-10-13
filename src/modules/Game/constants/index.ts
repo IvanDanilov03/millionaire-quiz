994f09bdca60c98a156a559a40f05c2bbbeba149
Ivan <ivd@empat.tech>
2025-10-14 00:21:57 +0300

    feat: add constants of the Game module

---
+++ src/modules/Game/constants/index.ts
@@ -0,0 +1,6 @@
+import questions from 'modules/Game/config/questions.json';
+import { GameConfig } from 'modules/Game/types';
+
+export const gameConfig: GameConfig = questions;
+
+export const ANSWER_LETTERS = ['A', 'B', 'C', 'D', 'E', 'F'];
