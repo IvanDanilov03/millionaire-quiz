830b6002119586279be0fef1f689caee01f39045
Ivan <ivd@empat.tech>
2025-10-14 02:43:30 +0300

    feat: add exports of QuestionScreen components

---
+++ src/modules/Game/components/QuestionScreen/components/index.ts
@@ -0,0 +1,17 @@
+export { AnswerOption } from './AnswerOption';
+export type { AnswerOptionProps, AnswerOptionStatus } from './AnswerOption';
+export { AnswerOptionGroup } from './AnswerOptionGroup';
+export {
+  MoneyLadderItem,
+  MoneyLadderGroup,
+  MoneyLadderHeader,
+  MoneyLadderDrawer,
+} from './MoneyLadder';
+export type {
+  MoneyLadderStep,
+  MoneyLadderItemProps,
+  MoneyLadderGroupProps,
+  MoneyLadderHeaderProps,
+  MoneyLadderDrawerProps,
+  MoneyLadderVariant,
+} from './MoneyLadder';
