b513bfad8b9346046b64fd17d742309eb7b15534
Ivan <ivd@empat.tech>
2025-10-14 02:42:23 +0300

    feat: create MoneyLadderGroup component

---
+++ src/modules/Game/components/QuestionScreen/components/MoneyLadder/MoneyLadderGroup/index.tsx
@@ -0,0 +1,48 @@
+import { FC, memo, useMemo } from 'react';
+
+import { MoneyLadderItem } from 'modules/Game/components/QuestionScreen/components/MoneyLadder/MoneyLadderItem';
+import type {
+  MoneyLadderStep,
+  MoneyLadderVariant,
+} from 'modules/Game/components/QuestionScreen/components/MoneyLadder/types';
+import classNames from 'utils/classNames';
+
+import styles from './styles.module.css';
+
+export interface MoneyLadderGroupProps {
+  items: MoneyLadderStep[];
+  variant?: MoneyLadderVariant;
+}
+
+export const MoneyLadderGroup: FC<MoneyLadderGroupProps> = memo(
+  ({ items, variant = 'desktop' }) => {
+    const sortedItems = useMemo(
+      () => [...items].sort((a, b) => b.amount - a.amount),
+      [items],
+    );
+
+    return (
+      <div className={classNames(styles.root, styles[variant])}>
+        <div
+          className={classNames(
+            styles.list,
+            variant === 'mobile' && styles.listMobile,
+          )}
+        >
+          {sortedItems.map((item) => (
+            <MoneyLadderItem
+              key={item.id}
+              amount={item.amount}
+              isCurrent={item.isCurrent}
+              isCompleted={item.isCompleted}
+            />
+          ))}
+        </div>
+      </div>
+    );
+  },
+);
+
+MoneyLadderGroup.displayName = 'MoneyLadderGroup';
+
+export default MoneyLadderGroup;
