8835ad0a4e84b678ee569b90db49f4caca608bc8
Ivan <ivd@empat.tech>
2025-10-14 02:43:02 +0300

    feat: add exports of MoneyLadder components

---
+++ src/modules/Game/components/QuestionScreen/components/MoneyLadder/index.ts
@@ -0,0 +1,14 @@
+export type { MoneyLadderStep, MoneyLadderVariant } from './types';
+export { MoneyLadderItem, type MoneyLadderItemProps } from './MoneyLadderItem';
+export {
+  MoneyLadderGroup,
+  type MoneyLadderGroupProps,
+} from './MoneyLadderGroup';
+export {
+  MoneyLadderHeader,
+  type MoneyLadderHeaderProps,
+} from './MoneyLadderHeader';
+export {
+  MoneyLadderDrawer,
+  type MoneyLadderDrawerProps,
+} from './MoneyLadderDrawer';
