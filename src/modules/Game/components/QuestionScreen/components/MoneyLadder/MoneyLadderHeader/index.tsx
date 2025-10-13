ea74b133f5f4bdc8f4401591207b408f4e4a69b4
Ivan <ivd@empat.tech>
2025-10-14 02:42:35 +0300

    feat: create MoneyLadderHeader component

---
+++ src/modules/Game/components/QuestionScreen/components/MoneyLadder/MoneyLadderHeader/index.tsx
@@ -0,0 +1,45 @@
+import { FC } from 'react';
+
+import closeIcon from 'assets/icons/close-icon.png';
+import classNames from 'utils/classNames';
+
+import { IconButton } from 'ui/IconButton';
+import { Image } from 'ui/Image';
+
+import styles from './styles.module.css';
+
+type MoneyLadderHeaderVariant = 'desktop' | 'mobile';
+
+export interface MoneyLadderHeaderProps {
+  variant: MoneyLadderHeaderVariant;
+  onClose?: () => void;
+  className?: string;
+}
+
+export const MoneyLadderHeader: FC<MoneyLadderHeaderProps> = ({
+  variant,
+  onClose,
+  className,
+}) => {
+  return (
+    <header className={classNames(styles.root, styles[variant], className)}>
+      {variant === 'mobile' && onClose && (
+        <IconButton
+          aria-label="Close prize ladder"
+          onClick={onClose}
+          size="small"
+        >
+          <Image
+            src={closeIcon.src}
+            alt=""
+            width={24}
+            height={24}
+            aria-hidden
+          />
+        </IconButton>
+      )}
+    </header>
+  );
+};
+
+export default MoneyLadderHeader;
